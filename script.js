78d6f0ccc84fd01deb99f787ca8182bed233ebeb
visualapps <visualapps.in@gmail.com>
2017-06-15 00:29:57 +0530

    first push
    12:29 Thu 15 jun

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function () {
+    $(".typed").typed({
+        strings: ["Data Analysts.", "Product updates.", "Entrepreneurs.", "Technologist."]
+        , typeSpeed: 50
+        , backDelay: 1000
+        , loop: true
+    });
+});
